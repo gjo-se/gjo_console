6e955ced67b573fc65c985c08024cc8fccbded9e
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2024-06-27 11:34:31 +0200

    added WIn hardlink

--- _unsortet/unsortet.sh
+++ _unsortet/unsortet.sh
@@ -14,7 +14,11 @@ https://docs.typo3.org/m/typo3/guide-installation/10.4/en-us/ExtensionInstallati
 
 # WIN Symlink
 cd C:\Users\gjo\Dropbox\5-Berufsleben\gjoSe\Development\Projects\tiger.de\packages
-sudo mklink /d gjo_console C:\Users\gjo\Dropbox\5-Berufsleben\gjoSe\Development\localRepositories\gjo_console
+# Softlink
+sudo mklink /d gjo_console C:\Users\gjo\Dropbox\5-Berufsleben\gjoSe\Development\localRepositories\ext\gjo\gjo_console
+# Hardlink
+sudo mklink /j gjo_site_package C:\Users\gjo\Dropbox\5-Berufsleben\gjoSe\Development\localRepositories\ext\gjo\gjo_site_package
+
 
 # WIN Powershell Symlink (funktioniert nicht für Composer)
 sudo New-Item -ItemType SymbolicLink -Path  C:\Users\gjo\Dropbox\5-Berufsleben\gjoSe\Development\Projects\tiger.de\packages\gjo_console -Target C:\Users\gjo\Dropbox\5-Berufsleben\gjoSe\Development\localRepositories\ext\gjo\gjo_console
